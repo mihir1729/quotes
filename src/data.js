8051987af93cce206deec346299b761aadbf1d4b
Mihir Yadav <72518307+mihir1729@users.noreply.github.com>
2022-08-26 13:41:54 +0530

    prev-next btn
    quotes project completed

---
+++ src/data.js
@@ -0,0 +1,44 @@
+const reviews = [
+	{
+		id: 1,
+		name: "kunal shah",
+		job: "serial entreprenuer",
+		image:
+			"https://pbs.twimg.com/profile_images/1190747917998546944/D3U5FNa7_400x400.jpg",
+		text: "How much you earn is rooted in where you spend time and money.",
+	},
+	{
+		id: 2,
+		name: "george bernard shaw",
+		job: "playwright",
+		image:
+			"https://d3d00swyhr67nd.cloudfront.net/h300/collection/GMIII/MCAG/GMIII_MCAG_1942_105-001.jpg",
+		text: "The single biggest problem in communication is the illusion that is has taken place.",
+	},
+	{
+		id: 3,
+		name: "adam grant",
+		job: "organizational psychologist",
+		image:
+			"https://pbs.twimg.com/profile_images/1427272847643316232/9CeNBJAr_400x400.jpg",
+		text: `Many people procrastinate because they're waiting for their motivation to rise. They forget that getting started is what leads their motivation to rise.`,
+	},
+	{
+		id: 4,
+		name: "socrates",
+		job: "philosopher",
+		image:
+			"https://upload.wikimedia.org/wikipedia/commons/thumb/b/bc/Socrate_du_Louvre.jpg/1200px-Socrate_du_Louvre.jpg",
+		text: "The secret of change is to focus all of your energy not on fighting the old, but on building the new.",
+	},
+	{
+		id: 5,
+		name: "naval ravikant",
+		job: "angel investor",
+		image:
+			"https://pbs.twimg.com/profile_images/1350045723073671169/xB1_K1_z_400x400.jpg",
+		text: "The biggest advantage of working remotely is that you no longer have to pretend to work 60 hours a week.",
+	},
+];
+
+export default reviews;
